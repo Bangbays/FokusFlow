77af79b47085d4ed42558aeee29fe29b50ac66ab
Bangbays <amirullahakbar@gmail.com>
2025-08-15 19:37:53 +0700

    Update page.tsx

--- src/app/project/[projectId]/page.tsx
+++ src/app/project/[projectId]/page.tsx
@@ -1,8 +1,5 @@
-// src/app/project/[projectId]/page.tsx
-
 import ProjectClientPage from "./ProjectClientPage";
 
 export default function Page() {
-  // Tidak ada props, tidak ada async, hanya merender Client Component
   return <ProjectClientPage />;
 }
