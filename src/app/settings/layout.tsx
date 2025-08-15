4a1764ded750b0688b75e77e05a59d3f1368c279
Bangbays <amirullahakbar@gmail.com>
2025-08-15 19:39:21 +0700

    Update layout.tsx

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,11 +1,9 @@
-// src/app/settings/layout.tsx
-"use client"; // Diperlukan karena menggunakan state untuk sidebar
+"use client";
 
 import React, { useState } from "react";
 import Header from "@/components/layout/Header";
 import Sidebar from "@/components/layout/Sidebar";
 
-// Ini adalah AppLayout yang sama yang digunakan oleh Dashboard dan Project
 export default function SettingsLayout({
   children,
 }: {
@@ -25,7 +23,6 @@ export default function SettingsLayout({
           isSidebarCollapsed ? "ml-16" : "ml-60"
         }`}
       >
-        {/* Konten dari page.tsx akan dirender di sini */}
         {children}
       </main>
     </div>
