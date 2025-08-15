1dd8ffe9aff558a192521797cea11cd4202ee759
Bangbays <amirullahakbar@gmail.com>
2025-08-15 20:07:28 +0700

    Update AppImage.tsx

--- src/components/AppImage.tsx
+++ src/components/AppImage.tsx
@@ -1,8 +1,7 @@
-"use client"; // Diperlukan karena adanya event handler onError
+"use client";
 
 import React from "react";
 
-// Definisikan tipe untuk props
 interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   src?: string;
   alt?: string;
@@ -14,9 +13,7 @@ export default function Image({
   className = "",
   ...props
 }: ImageProps) {
-  // Event handler untuk gambar yang gagal dimuat
   const handleError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
-    // Mengganti sumber gambar dengan gambar placeholder
     (e.target as HTMLImageElement).src = "/assets/images/no_image.png";
   };
 
